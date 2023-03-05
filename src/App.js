2c4b99d7feae944bb93f943ea88580290cd01333
Mark Gil P. Rusiana <markgilrusiana437@gmail.com>
2023-03-05 15:57:44 +0800

    some changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Home } from './screens/home/home';
 import { Library } from './screens/library/library';
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="libary" element={<Library />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
